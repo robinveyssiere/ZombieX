49990de84bae9aae8c3ff46f63b1cc98157b0284
Robi-Lolin <robin.veyssiere@epitech.eu>
2020-02-01 16:18:15 +0100

    Added movement script
    je vous aime

---
+++ Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public float MovementSpeed;
+
+    void Update()
+    {
+        Vector3 direction = CalculateDirection();
+        transform.Translate(direction * MovementSpeed * Time.deltaTime);
+    }
+
+    public Vector3 CalculateDirection()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            direction.y += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            direction.x -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            direction.y -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            direction.x += 1.0f;
+        }
+        return direction.normalized;
+    }
+}
