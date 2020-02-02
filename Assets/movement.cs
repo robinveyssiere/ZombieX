b14100065eb98c50c4c397ab62d112672fc422a5
Robi-Lolin <robin.veyssiere@epitech.eu>
2020-02-02 12:26:37 +0100

    Added zombie spawn random 4 position + wall hitbox
    ouais...

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,14 +1,49 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class movement : MonoBehaviour
 {
 
+    public List<GameObject> goList;
     public float MovementSpeed;
+    public GameObject zombie;
+    public float spawn_speed;
+
+    void Start()
+    {
+        goList = new List<GameObject>();
+    }
 
     void Update()
     {
+        spawn_speed -= Time.deltaTime;
         Vector3 direction = CalculateDirection();
         transform.Translate(direction * MovementSpeed * Time.deltaTime);
+        if (spawn_speed <= 0f && goList.Count < 5)
+        {
+            int rnd = Random.Range(1, 5);
+            Vector3 rndPos1 = new Vector3(-6, 0, 0);
+            Vector3 rndPos2 = new Vector3(4, 0, 0);
+            Vector3 rndPos3 = new Vector3(0, -4, 0);
+            Vector3 rndPos4 = new Vector3(0, 4, 0);
+            if (rnd == 1)
+            {
+                goList.Add((GameObject)Instantiate(zombie, rndPos1, Quaternion.identity));
+            }
+            else if (rnd == 2)
+            {
+                goList.Add((GameObject)Instantiate(zombie, rndPos2, Quaternion.identity));
+            }
+            else if (rnd == 3)
+            {
+                goList.Add((GameObject)Instantiate(zombie, rndPos3, Quaternion.identity));
+            }
+            else if (rnd == 4)
+            {
+                goList.Add((GameObject)Instantiate(zombie, rndPos4, Quaternion.identity));
+            }
+            spawn_speed = 5f;
+        }
     }
 
     public Vector3 CalculateDirection()
